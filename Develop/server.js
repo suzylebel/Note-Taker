24a0ffdd92abeb52600dc4e9776ecc06cec7d43f
Suzy Le Bel <suzy.lebel@gmail.com>
2020-12-30 15:16:45 -0800

    updates to server.js

--- Develop/server.js
+++ Develop/server.js
@@ -1,63 +1,84 @@
-const express = require("express");
-// const fs = require('fs');
-const db = require('./db/db.json')
-// const path = require('path');
 
-// express server
-const app = express();
-const PORT = process.env.PORT || 3030;
+//package dependencies 
+var express = require("express");
+var path = require("path");
+var fs = require("fs")
+const notesdb = require("./db/db.json");
+const json = require("express");
 
-// data parse
+// starts the App
+var app = express();
+var PORT = process.env.PORT || 3030;
+
+// Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("public"));
+app.use(express.static('public'))
 
-require("./routes/htmlRoutes")(app);
-require("./routes/apiRoutes")(app);
+//API ROUTES 
+app.get("/api/notes", function(req,res){
+  return res.json(notesdb)
+})
 
-// const htmlRoutes = require('./routes/htmlRoutes');
-// app.use('/', htmlRoutes); 
+app.post("/api/notes", function(req,res){
+  let newNote = req.body
+  let newnoteDB = notesdb
+  let noteId = Date.now()
+  // grab by the id
+  newNote["id"] = noteId
+  newnoteDB.push(newNote)
+  fs.writeFile("./db/db.json", JSON.stringify(newnoteDB), function(err) {
+    if (err) {
+        return console.log(err);
+    }
+    console.log("New note added!");
+    })
+  res.json(true)
+})
 
-// const apiRoutes = require('./routes/apiRoutes');
-// app.use('/api/notes', apiRoutes); 
+app.delete("/api/notes/:id", (req, res) =>
+{
+  let deletedId = parseInt(req.params.id)
+  let newnoteDB = notesdb
+  let noteIndex = ""
+  function getdelnote(){
+    // delete by the id 
+    newnoteDB.forEach(note=> {
+      if (note.id === deletedId){
+        noteIndex = newnoteDB.indexOf(note) 
+      }
+    })
+  }
+  getdelnote()
+  newnoteDB.splice(noteIndex, 1);
+  fs.writeFile("./db/db.json", JSON.stringify(newnoteDB), function(err) {
+    if (err) {
+        return console.log(err);
+    }
+    console.log("Note successfully deleted!");
+    })
+  res.json(true)
+})
 
+// HTML Routes (pulled here instead of file)
+// =============================================================
 
-app.listen(PORT, function() {
-  console.log("App listening on PORT: " + PORT);
+//sends user to note page
+app.get("/notes", function(req, res) {
+  res.sendFile(path.join(__dirname, "public/notes.html"));
+});
+//sends user to homepage
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "public/index.html"));
+});
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
 
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
 
-
-
-
-// // API Routing
-// app.get("/api/notes", function (req, res) {
-//   return res.json(db);
-// });
-
-// app.post("/api/notes", function (req, res) {
-//   const newNote = req.body;
-//   newNote.id = new Date().getTime().toString();
-//   db.push(newNote);
-//   fs.writeFileSync('./db/db.json', JSON.stringify(db));
-//   return res.json(newNote);
-// });
-
-// app.delete('/api/notes/:id', function (req, res) {
-//   const id = req.params.id;
-//   const index = db.findIndex(function (note) {
-//     return note.id === id;
-//   })
-//   if (index !== -1) {
-//     db.splice(index, 1)
-//   }
-//   fs.writeFileSync('./db/db.json', JSON.stringify(db));
-//   return res.json(id);
-// })
-
-
-// // Listen on PORT:
-// app.listen(PORT, function () {
-//   console.log("App listening on PORT " + PORT);
-// });
